0da1c2490413bcac0ec513d5f4813d1605391312
kevin-sleeckx <kevin-sleeckx@users.noreply.github.com>
2025-06-11 11:08:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 let currentDate = new Date();
 
 // Version management
-const CURRENT_VERSION = '1.5.6';
+const CURRENT_VERSION = '1.5.7';
 const LAST_VERSION_KEY = 'app_version';
 
 // Update version display in the UI
